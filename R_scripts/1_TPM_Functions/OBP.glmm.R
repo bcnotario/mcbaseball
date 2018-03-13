0e2ff03f7aa7e16855e72ca8fc744044bf3c8f53
Brandon N <37309728+bcnotario@users.noreply.github.com>
2018-03-13 01:26:40 -0700

    Update OBP.glmm.R

--- R_scripts/1_TPM_Functions/OBP.glmm.R
+++ R_scripts/1_TPM_Functions/OBP.glmm.R
@@ -1,6 +1,6 @@
 #OBP Pitcher & Situation Adjustment by GLMM for Run TPM
 #Inputs: pitcher (Pitcher ID), outs (Out Situation), baserun (Base Runner Situation), glmm (GLMM)
-#Packages: 
+#Packages: faraway
 
 OBP.glmm <- function(pitcher,outs,baserun,glmm){
   ilogit(predict(glmm,newdata=data.frame(PIT_ID=pitcher,OUTS_CT=outs,BASE_STATE=baserun)))/
